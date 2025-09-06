1fe44055370082f90b687a6e369d9008838b7262
Kendrick Balanquit <kendrick.balanquit@accenture.com>
2025-09-06 21:13:41 +0800

    Updated components and navbar

--- src/app/users/[username]/page.tsx
+++ src/app/users/[username]/page.tsx
@@ -1,3 +1,5 @@
+"use client";
+
 import CardList from "@/components/CardList";
 import { Badge } from "@/components/ui/badge";
 import {
@@ -8,9 +10,18 @@ import {
   BreadcrumbPage,
   BreadcrumbSeparator,
 } from "@/components/ui/breadcrumb";
-import { HoverCard, HoverCardContent, HoverCardTrigger } from "@/components/ui/hover-card";
+import {
+  HoverCard,
+  HoverCardContent,
+  HoverCardTrigger,
+} from "@/components/ui/hover-card";
 import { Progress } from "@/components/ui/progress";
 import { BadgeCheck, Candy, Citrus, Shield } from "lucide-react";
+import { Sheet, SheetTrigger } from "@/components/ui/sheet";
+import { Button } from "@/components/ui/button";
+import EditUser from "@/components/EditUser";
+import { Avatar, AvatarFallback, AvatarImage } from "@/components/ui/avatar";
+import AppLineChart from "@/components/AppLineChart";
 
 const SingeUserPage = () => {
   return (
@@ -39,11 +50,16 @@ const SingeUserPage = () => {
             <div className="flex gap-4 mt-4">
               <HoverCard>
                 <HoverCardTrigger>
-                  <BadgeCheck size={36} className="rounded-full bg-blue-500/30 border-1 border-blue-500/50 p-2"/>
+                  <BadgeCheck
+                    size={36}
+                    className="rounded-full bg-blue-500/30 border-1 border-blue-500/50 p-2"
+                  />
                 </HoverCardTrigger>
                 <HoverCardContent>
                   <h1 className="font-bold mb-2">Verified User</h1>
-                  <p className="text-sm text-muted-foreground">This user has been verified by the admin.</p>
+                  <p className="text-sm text-muted-foreground">
+                    This user has been verified by the admin.
+                  </p>
                 </HoverCardContent>
               </HoverCard>
               <HoverCard>
@@ -93,10 +109,20 @@ const SingeUserPage = () => {
           </div>
           {/* INFORMATION CONTAINER */}
           <div className="bg-primary-foreground p-4 rounded-lg">
-            <h1 className="text-xl font-semibold">User Information</h1>
+            <div className="flex items-center justify-between">
+              <h1 className="text-xl font-semibold">User Information</h1>
+              <Sheet>
+                <SheetTrigger asChild>
+                  <Button>Edit User</Button>
+                </SheetTrigger>
+                <EditUser />
+              </Sheet>
+            </div>
             <div className="space-y-4 mt-4">
               <div className="flex flex-col gap-2 mb-8">
-                <p className="text-sm text-muted-foreground">Profile completion</p>
+                <p className="text-sm text-muted-foreground">
+                  Profile completion
+                </p>
                 <Progress value={70} />
               </div>
               <div className="flex items-center gap-2">
@@ -120,7 +146,9 @@ const SingeUserPage = () => {
                 <Badge>Admin</Badge>
               </div>
             </div>
-            <p className="text-sm text-muted-foreground mt-4">Joined on 2025.01.01</p>
+            <p className="text-sm text-muted-foreground mt-4">
+              Joined on 2025.01.01
+            </p>
           </div>
           {/* CARD LIST CONTAINER */}
           <div className="bg-primary-foreground p-4 rounded-lg">
@@ -130,9 +158,27 @@ const SingeUserPage = () => {
         {/* RIGHT */}
         <div className="w-full xl:w-2/3 space-y-6">
           {/* USER CARD CONTAINER */}
-          <div className="bg-primary-foreground p-4 rounded-lg">User Card</div>
+          <div className="bg-primary-foreground p-4 rounded-lg space-y-2">
+            <div className="flex items-center gap-2">
+              <Avatar className="size-12">
+                <AvatarImage src="https://github.com/shadcn.png" />
+                <AvatarFallback>JD</AvatarFallback>
+              </Avatar>
+
+              <h1 className="text-xl font-semibold">John Doe</h1>
+            </div>
+            <p className="text-sm text-muted-foreground">
+              Lorem ipsum dolor, sit amet consectetur adipisicing elit.
+              Cupiditate magnam distinctio doloremque nesciunt fugiat provident,
+              repellendus quisquam non debitis quia autem rem optio possimus,
+              repellat eveniet quo minus libero ex.
+            </p>
+          </div>
           {/* CHART CONTAINER */}
-          <div className="bg-primary-foreground p-4 rounded-lg">Chart</div>
+          <div className="bg-primary-foreground p-4 rounded-lg space-y-2">
+            <h1 className="text-xl font-semibold">User Activity</h1>
+            <AppLineChart />
+          </div>
         </div>
       </div>
     </div>
--- src/components/Navbar.tsx
+++ src/components/Navbar.tsx
@@ -17,9 +17,9 @@ import { SidebarTrigger, useSidebar } from "./ui/sidebar";
 
 const Navbar = () => {
   const { theme, setTheme } = useTheme();
-  const {toggleSidebar} = useSidebar()
+  const { toggleSidebar } = useSidebar();
   return (
-    <nav className="p-4 flex items-center justify-between">
+    <nav className="p-4 flex items-center justify-between sticky top-0 bg-background z-10">
       {/* LEFT */}
       <SidebarTrigger />
       {/* <Button variant="outline" onClick={toggleSidebar}>Custom Button</Button> */}
@@ -28,25 +28,25 @@ const Navbar = () => {
         <Link href="/">Dashboard</Link>
         {/* THEME MENU */}
         <DropdownMenu>
-      <DropdownMenuTrigger asChild>
-        <Button variant="outline" size="icon">
-          <Sun className="h-[1.2rem] w-[1.2rem] scale-100 rotate-0 transition-all dark:scale-0 dark:-rotate-90" />
-          <Moon className="absolute h-[1.2rem] w-[1.2rem] scale-0 rotate-90 transition-all dark:scale-100 dark:rotate-0" />
-          <span className="sr-only">Toggle theme</span>
-        </Button>
-      </DropdownMenuTrigger>
-      <DropdownMenuContent align="end">
-        <DropdownMenuItem onClick={() => setTheme("light")}>
-          Light
-        </DropdownMenuItem>
-        <DropdownMenuItem onClick={() => setTheme("dark")}>
-          Dark
-        </DropdownMenuItem>
-        <DropdownMenuItem onClick={() => setTheme("system")}>
-          System
-        </DropdownMenuItem>
-      </DropdownMenuContent>
-    </DropdownMenu>
+          <DropdownMenuTrigger asChild>
+            <Button variant="outline" size="icon">
+              <Sun className="h-[1.2rem] w-[1.2rem] scale-100 rotate-0 transition-all dark:scale-0 dark:-rotate-90" />
+              <Moon className="absolute h-[1.2rem] w-[1.2rem] scale-0 rotate-90 transition-all dark:scale-100 dark:rotate-0" />
+              <span className="sr-only">Toggle theme</span>
+            </Button>
+          </DropdownMenuTrigger>
+          <DropdownMenuContent align="end">
+            <DropdownMenuItem onClick={() => setTheme("light")}>
+              Light
+            </DropdownMenuItem>
+            <DropdownMenuItem onClick={() => setTheme("dark")}>
+              Dark
+            </DropdownMenuItem>
+            <DropdownMenuItem onClick={() => setTheme("system")}>
+              System
+            </DropdownMenuItem>
+          </DropdownMenuContent>
+        </DropdownMenu>
         {/* USER MENU */}
         <DropdownMenu>
           <DropdownMenuTrigger>
